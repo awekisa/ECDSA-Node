a5ca287c960d68753b76e95cd09cb09bdea079e2
dimitar.valchev <dimitar.valtchev@gmail.com>
2023-07-30 14:51:41 +0300

    add .env file

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -2,18 +2,23 @@ const secp = require("ethereum-cryptography/secp256k1.js");
 const { toHex } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 
-const privateKey = secp.secp256k1.utils.randomPrivateKey();
-console.log('private key 1:', toHex(privateKey));
+require('dotenv').config();
 
-// const publicKey = keccak256(secp.secp256k1.getPublicKey(privateKey).slice(1).slice(-20));
-// console.log('public key 1:', toHex(publicKey));
-// console.log(Object.getOwnPropertyNames(secp.secp256k1))
-const pub = secp.secp256k1.getPublicKey(privateKey);
-const msg = new TextEncoder().encode('hello');
-const sig = secp.secp256k1.sign(msg, privateKey);
-const isValid = secp.secp256k1.verify(sig, msg, pub);
+const apiKey = process.env.API_KEY
+console.log(apiKey)
 
-console.log('private key:', toHex(privateKey));
-console.log('public key:', toHex(pub));
-console.log(sig);
-console.log(isValid);
+// const privateKey = secp.secp256k1.utils.randomPrivateKey();
+// console.log('private key 1:', toHex(privateKey));
+
+// // const publicKey = keccak256(secp.secp256k1.getPublicKey(privateKey).slice(1).slice(-20));
+// // console.log('public key 1:', toHex(publicKey));
+// // console.log(Object.getOwnPropertyNames(secp.secp256k1))
+// const pub = secp.secp256k1.getPublicKey(privateKey);
+// const msg = new TextEncoder().encode('hello');
+// const sig = secp.secp256k1.sign(msg, privateKey);
+// const isValid = secp.secp256k1.verify(sig, msg, pub);
+
+// console.log('private key:', toHex(privateKey));
+// console.log('public key:', toHex(pub));
+// console.log(sig);
+// console.log(isValid);
